8539d90a5ee1e841c6594812526a9c4a33ae4ab4
Vinicius Moreira <vinicius.m@einstein.br>
2024-03-16 20:42:24 -0300

    fix: adjusting database files

--- main.tf
+++ main.tf
@@ -3,33 +3,12 @@ provider "aws" {
   region = var.region
 }
 
-data "aws_availability_zones" "available" {}
-
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "2.77.0"
-
-  name                 = "fiap"
-  cidr                 = "10.0.0.0/16"
-  azs                  = data.aws_availability_zones.available.names
-  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
-  enable_dns_hostnames = true
-  enable_dns_support   = true
+data "aws_vpc" "default" {
+  default = true
 }
 
-resource "aws_db_subnet_group" "fiap" {
-  name       = "fiap"
-  subnet_ids = module.vpc.public_subnets
-
-  tags = {
-    Name = "fiap"
-  }
-}
-
-resource "aws_security_group" "rds" {
-  name   = "fiap_rds"
-  vpc_id = module.vpc.vpc_id
-
+resource "aws_default_security_group" "default" {
+  vpc_id = data.aws_vpc.default.id
   ingress {
     from_port   = 3306
     to_port     = 3306
@@ -45,37 +24,21 @@ resource "aws_security_group" "rds" {
   }
 
   tags = {
-    Name = "fiap_rds"
+    Name = "RDS Mysql"
   }
 }
 
-resource "aws_db_parameter_group" "fiap" {
-  name   = "fiap"
-  family = "mysql8.0"
-
-  parameter {
-    name  = "character_set_server"
-    value = "utf8"
-  }
-
-  parameter {
-    name  = "character_set_client"
-    value = "utf8"
-  }
-}
-
-resource "aws_db_instance" "fiap" {
+resource "aws_db_instance" "mysql_db_instance" {
   identifier             = "fiap"
   instance_class         = "db.t3.micro"
   allocated_storage      = 15
   max_allocated_storage  = 30
+  db_name                = var.db_name
   engine                 = "mysql"
   engine_version         = "8.0"
   username               = var.db_user
   password               = var.db_password
-  db_subnet_group_name   = aws_db_subnet_group.fiap.name
-  vpc_security_group_ids = [aws_security_group.rds.id]
-  parameter_group_name   = aws_db_parameter_group.fiap.name
-  publicly_accessible    = true
+  publicly_accessible    = true # change to false in prod
   skip_final_snapshot    = true
+  vpc_security_group_ids = [aws_default_security_group.default.id]
 }
--- variables.tf
+++ variables.tf
@@ -3,13 +3,18 @@ variable "region" {
   default     = "us-east-1"
   description = "AWS region"
 }
+variable "db_name" {
+  description = "RDS database name"
+  sensitive   = true
+}
 
 variable "db_user" {
   description = "RDS username"
   sensitive   = true
 }
 
+
 variable "db_password" {
-  description = "RDS user password"
+  description = "RDS root user password"
   sensitive   = true
 }
